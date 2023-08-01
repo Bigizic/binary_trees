687e565c9490e4cba8e363ad72e2770e205d4916
Bigizic <olalekanisaac75@gmail.com>
2023-08-01 13:17:59 +0000

    Update binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - a function that checks if a binary tree is a leaf
+ *
+ * @node: pointer to node to check
+ *
+ * Return: 1 if node is a leaf otherwise 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left != NULL && node->right != NULL)
+		return (1);
+
+	return (0);
+}
