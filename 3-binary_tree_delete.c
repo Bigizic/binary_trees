1d70fdecf5c537cca3d03fea8ea7fa85be6e086f
Bigizic <olalekanisaac75@gmail.com>
2023-08-01 13:17:50 +0000

    Update binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - a function that frees an enitre binary tree
+ *
+ * @tree: pointer to the tree to free
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	/* recursively delete tree left and right nodes */
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+
+	return;
+}
