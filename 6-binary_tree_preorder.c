109ca669d87f5e2f64cad908118ac664fa9767d5
Bigizic <olalekanisaac75@gmail.com>
2023-08-01 14:50:15 +0000

    Update binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_preorder - a function that prints nodes data using
+ * pre-order travasal method
+ *
+ * @tree: pointer to binary tree to be printed
+ *
+ * @func: pointer to a function to call for each node
+ *
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
