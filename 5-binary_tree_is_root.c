32490588a4840953beb71be044268186a3370620
Bigizic <olalekanisaac75@gmail.com>
2023-08-01 13:22:09 +0000

    Update binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,7 +13,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left != NULL && node->right != NULL)
+	if (node->left != NULL || node->right != NULL)
 		return (1);
 
 	return (0);
