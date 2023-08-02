96f858726ccc71ac92edc932bf026bb4e24bed89
Bigizic <olalekanisaac75@gmail.com>
2023-08-02 17:55:39 +0000

    Update binary_tree_is_complete.c

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,68 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - a function that checks if a binary tree is full\
+ *
+ * @tree: tree to check
+ *
+ * Return: 1 if tree is full otherwise 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - a function that measures the height of a binary
+ * tree
+ *
+ * @tree: tree to be measured
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL || binary_tree_is_full(tree))
+		return (0);
+
+	right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+
+	return (1 + (left >= right ? left : right));
+}
+
+/**
+ * binary_tree_is_complete - a function that checks if a binary tree
+ * is complete
+ *
+ * @ tree: tree to check
+ *
+ * Return: 1 if tree is complete otherwise 0
+ */
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_full(tree))
+		return (1);
+
+	if (tree->left == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (binary_tree_is_complete(tree->left) &&
+				binary_tree_is_complete(tree->right))
+			return (1);
+	}
+	return (0);
+}
