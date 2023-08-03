338628ce320137753e331201657655533c4b9b08
Bigizic <olalekanisaac75@gmail.com>
2023-08-03 20:20:24 +0000

    Update bst_search.c

---
+++ 113-bst_search.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - a function that searches for a value in a Binary
+ * Search Tree
+ *
+ * @tree: tree to search from
+ *
+ * @value: value to look for
+ *
+ * Return: pointer to the node that contain value
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	bst_t *radar = NULL;
+
+	if (tree == NULL)
+		return (NULL);
+
+	radar = (bst_t *)tree;
+
+	while (radar->n != value)
+	{
+		if (radar != NULL)
+		{
+			if (radar->n > value)
+				radar = radar->left;
+			else
+				radar = radar->right;
+
+			if (radar == NULL)
+				return (NULL);
+		}
+	}
+	return (radar);
+}
