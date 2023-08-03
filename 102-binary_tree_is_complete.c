dafbf6eaf97d8ee684807b219e034f54cf388a6c
Bigizic <olalekanisaac75@gmail.com>
2023-08-03 16:00:06 +0000

    Update binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -17,6 +17,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	return (0);
 }
 
+
 /**
  * binary_tree_height - a function that measures the height of a binary
  * tree
@@ -29,7 +30,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || binary_tree_is_full(tree))
+	if (tree == NULL)
 		return (0);
 
 	right = binary_tree_height(tree->right);
@@ -37,6 +38,36 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (1 + (left >= right ? left : right));
 }
+
+
+/**
+ * binary_tree_is_perfect - a function that checks if a binary tree
+ * is perfect
+ *
+ * @tree: pointer to tree to check
+ *
+ * Return: 1 if perfect otherwise 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (binary_tree_is_full(tree))
+		return (1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right))
+			return (1);
+	}
+	return (0);
+}
+
 
 /**
  * binary_tree_is_complete - a function that checks if a binary tree
@@ -49,19 +80,27 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	size_t left, right;
+
 	if (tree == NULL)
 		return (0);
 
 	if (binary_tree_is_full(tree))
 		return (1);
+	
+	left = binary_tree_height(tree->left);
+	right =  binary_tree_height(tree->right);
 
-	if (tree->left == NULL)
-		return (0);
-
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	if (left == right)
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_complete(tree->right))
+			return (1);
+	}
+	else if (left == right + 1)
 	{
 		if (binary_tree_is_complete(tree->left) &&
-				binary_tree_is_complete(tree->right))
+				binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
